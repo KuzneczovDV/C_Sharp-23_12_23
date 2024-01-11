bb4ed3bc59c3ffea0552bf05bf13d529132ff61a
Dmitrii <kuzneczovdv@gmail.com>
2024-01-11 05:47:08 +0300

    add lesson2: task4

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] array = { 21, 4, 34, 3, 9 };
+int i = 0;
+int max = array[0];
+while(i < n)
+{
+  if(array[i] > max)
+  {
+    max = array[i];
+  }
+  i = i + 1;
+}
+Console.WriteLine(max);
